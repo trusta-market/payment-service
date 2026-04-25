441829132dd65200f7d5dd4a74c961c487294466
zlon <zlon@outlook.kr>
2026-04-26 02:55:06 +0900

    refactor: wildcard import 설정 변경

--- src/main/java/com/trustamarket/paymentservice/paymentservice/domain/entity/PaymentTx.java
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/domain/entity/PaymentTx.java
@@ -1,6 +1,14 @@
 package com.trustamarket.paymentservice.paymentservice.domain.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
