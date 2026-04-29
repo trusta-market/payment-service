839926e20d37dbe93552f9026c03520600e40a24
zlon <zlon@outlook.kr>
2026-04-30 02:43:29 +0900

    refactor: 필드 누락 수정

--- src/main/java/com/trustamarket/paymentservice/paymentservice/application/dto/result/SucceededPaymentResult.java
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/application/dto/result/SucceededPaymentResult.java
@@ -16,6 +16,7 @@ public record SucceededPaymentResult(
         return new SucceededPaymentResult(
                 payment.getPaymentId(),
                 payment.getPaymentStatus(),
+                payment.getAmount(),
                 payment.getUpdatedAt()
         );
     }
