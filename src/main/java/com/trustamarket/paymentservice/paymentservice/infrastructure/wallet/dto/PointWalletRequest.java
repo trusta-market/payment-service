d8e0b79bfc0843b7765ffc8468621601a34c0cd7
zlon <zlon@outlook.kr>
2026-05-01 16:34:56 +0900

    refactor: 검증로직 추가

--- src/main/java/com/trustamarket/paymentservice/paymentservice/infrastructure/wallet/dto/PointWalletRequest.java
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/infrastructure/wallet/dto/PointWalletRequest.java
@@ -1,8 +1,21 @@
 package com.trustamarket.paymentservice.paymentservice.infrastructure.wallet.dto;
 
+import com.trustamarket.paymentservice.paymentservice.domain.exception.PaymentErrorCode;
+import com.trustamarket.paymentservice.paymentservice.domain.exception.PaymentException;
+
 import java.util.UUID;
 
 public record PointWalletRequest (
     UUID paymentId,
     long chargedAmount
-){}
+){
+    public PointWalletRequest {
+        if(paymentId == null) {
+            throw new PaymentException(PaymentErrorCode.INVALID_PAYMENT_KEY);
+        }
+        if(chargedAmount <= 0) {
+            //todo : long -> Amount
+            throw new IllegalArgumentException("결제 금액은 0보다 커야 합니다.");
+        }
+    }
+}
