c9a95bb5e269e58ca1cf98fdea8e9fa96dbb6b5f
zlon <zlon@outlook.kr>
2026-05-07 17:02:44 +0900

    feat: 조회 repository

--- src/main/java/com/trustamarket/paymentservice/paymentservice/infrastructure/repository/PaymentJpaRepository.java
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/infrastructure/repository/PaymentJpaRepository.java
@@ -2,9 +2,9 @@ package com.trustamarket.paymentservice.paymentservice.infrastructure.repository
 
 import com.trustamarket.paymentservice.paymentservice.domain.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface PaymentJpaRepository extends JpaRepository<Payment, UUID> {
-    boolean existsByPaymentId(UUID paymentId);
+public interface PaymentJpaRepository extends JpaRepository<Payment, UUID>, JpaSpecificationExecutor<Payment> {
 }
--- src/main/java/com/trustamarket/paymentservice/paymentservice/infrastructure/repository/PaymentRepositoryAdapter.java
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/infrastructure/repository/PaymentRepositoryAdapter.java
@@ -1,10 +1,14 @@
 package com.trustamarket.paymentservice.paymentservice.infrastructure.repository;
 
+import com.trustamarket.paymentservice.paymentservice.application.dto.query.SearchPaymentQuery;
 import com.trustamarket.paymentservice.paymentservice.domain.entity.Payment;
 import com.trustamarket.paymentservice.paymentservice.domain.exception.PaymentErrorCode;
 import com.trustamarket.paymentservice.paymentservice.domain.exception.PaymentException;
 import com.trustamarket.paymentservice.paymentservice.domain.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -14,11 +18,6 @@ import java.util.UUID;
 public class PaymentRepositoryAdapter implements PaymentRepository {
 
     private final PaymentJpaRepository paymentJpaRepository;
-
-    @Override
-    public boolean existsByChargeId(UUID paymentId) {
-        return paymentJpaRepository.existsByPaymentId(paymentId);
-    }
 
     @Override
     public Payment saveAndFlush(Payment payment) {
@@ -31,5 +30,17 @@ public class PaymentRepositoryAdapter implements PaymentRepository {
         Payment payment = paymentJpaRepository.findById(paymentId).
                 orElseThrow(() -> new PaymentException(PaymentErrorCode.PAYMENT_NOT_FOUND));
         return payment;
+    }
+
+    @Override
+    public Slice<Payment> searchPayments(SearchPaymentQuery query, Pageable pageable) {
+        Specification<Payment> spec = PaymentSpecification.hasUserId(query.paymentId())
+                .and(PaymentSpecification.hasPaymentId(query.paymentId()))
+                .and(PaymentSpecification.hasStatusEq(query.status()))
+                .and(PaymentSpecification.amountBetween(query.minAmount(), query.maxAmount()));
+
+        paymentJpaRepository.findAll(spec, pageable);
+
+        return paymentJpaRepository.findAll(spec, pageable);
     }
 }
