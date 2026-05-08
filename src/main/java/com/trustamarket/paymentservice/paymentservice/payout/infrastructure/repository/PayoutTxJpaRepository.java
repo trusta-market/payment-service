27c3ac39307858ec85767f23c1f0751486be54a3
zlon <zlon@outlook.kr>
2026-05-08 16:48:03 +0900

    refactor: PayoutTxJpaRepository

--- src/main/java/com/trustamarket/paymentservice/paymentservice/payout/infrastructure/repository/PayoutTxJpaRepository.java
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/payout/infrastructure/repository/PayoutTxJpaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface PaymentTxJpaRepository extends JpaRepository<PayoutTx, UUID>{
+public interface PayoutTxJpaRepository extends JpaRepository<PayoutTx, UUID> {
 }
