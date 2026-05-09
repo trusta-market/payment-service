f4ff071867922807bcb7a7a60c7b711c6d667a07
zlon <zlon@outlook.kr>
2026-05-09 09:41:09 +0900

    feat: payout 생성 Service

---
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/payout/application/service/PayoutService.java
@@ -0,0 +1,27 @@
+package com.trustamarket.paymentservice.paymentservice.payout.application.service;
+
+import com.trustamarket.paymentservice.paymentservice.payment.domain.vo.Amount;
+import com.trustamarket.paymentservice.paymentservice.payout.application.dto.command.CreatePayoutCommand;
+import com.trustamarket.paymentservice.paymentservice.payout.application.dto.result.CreatePayoutResult;
+import com.trustamarket.paymentservice.paymentservice.payout.application.port.PayoutUseCase;
+import com.trustamarket.paymentservice.paymentservice.payout.domain.entity.Payout;
+import com.trustamarket.paymentservice.paymentservice.payout.domain.repository.PayoutRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PayoutService implements PayoutUseCase {
+
+    private final PayoutRepository payoutRepository;
+
+    @Override
+    @Transactional
+    public CreatePayoutResult createPayout(CreatePayoutCommand command) {
+            Payout payout = Payout.create(command.userId(), command.pointTxHistoryId(), Amount.of(command.withdrawAmount()));
+            Payout savedPayout = payoutRepository.saveAndFlush(payout);
+
+            return CreatePayoutResult.from(savedPayout);
+    }
+}
--- src/main/java/com/trustamarket/paymentservice/paymentservice/payout/domain/repository/PayoutRepository.java
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/payout/domain/repository/PayoutRepository.java
@@ -1,4 +1,7 @@
 package com.trustamarket.paymentservice.paymentservice.payout.domain.repository;
 
+import com.trustamarket.paymentservice.paymentservice.payout.domain.entity.Payout;
+
 public interface PayoutRepository {
+    Payout saveAndFlush(Payout payout);
 }
