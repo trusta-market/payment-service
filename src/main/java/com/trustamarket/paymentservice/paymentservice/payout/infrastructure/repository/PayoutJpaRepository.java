b5788ba723b5e216b5d4e12366d7b801a52e45ec
zlon <zlon@outlook.kr>
2026-05-10 19:43:26 +0900

    refactor: 재귀문제 수정

--- src/main/java/com/trustamarket/paymentservice/paymentservice/payout/infrastructure/repository/PayoutJpaRepository.java
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/payout/infrastructure/repository/PayoutJpaRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface PayoutJpaRepository extends JpaRepository<Payout, UUID> {
+    boolean existsByPointTxHistory(UUID pointTxRequestHistoryId);
 }
--- src/main/java/com/trustamarket/paymentservice/paymentservice/payout/infrastructure/repository/PayoutRepositoryAdapter.java
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/payout/infrastructure/repository/PayoutRepositoryAdapter.java
@@ -1,6 +1,8 @@
 package com.trustamarket.paymentservice.paymentservice.payout.infrastructure.repository;
 
 import com.trustamarket.paymentservice.paymentservice.payout.domain.entity.Payout;
+import com.trustamarket.paymentservice.paymentservice.payout.domain.exception.PayoutErrorCode;
+import com.trustamarket.paymentservice.paymentservice.payout.domain.exception.PayoutException;
 import com.trustamarket.paymentservice.paymentservice.payout.domain.repository.PayoutRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -21,11 +23,13 @@ public class PayoutRepositoryAdapter implements PayoutRepository {
 
     @Override
     public Payout findById(UUID payoutId) {
-        return findById(payoutId);
+        Payout payout = payoutJpaRepository.findById(payoutId)
+                .orElseThrow(()-> new IllegalArgumentException("출금요청 내용이 존재하지 않습니다."));
+        return payout;
     }
 
     @Override
     public boolean existsByPointTxHistory(UUID pointTxRequestHistoryId) {
-        return existsByPointTxHistory(pointTxRequestHistoryId);
+        return payoutJpaRepository.existsByPointTxHistory(pointTxRequestHistoryId);
     }
 }
