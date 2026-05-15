d8be4ae51f9d93f6c46b78a51d9bd08d58ba1f36
지원 <140367464+zlonce@users.noreply.github.com>
2026-05-15 17:32:33 +0900

    [refactor] 계좌조회 url 수정, mock 시간 변경 (#32)
    * refactor: mock 시간 수정
    
    * refactor: 유저 계좌조회 url 수정
    
    * refactor: userId 추가
    
    * refactor: userId 검증추가

--- src/main/java/com/trustamarket/paymentservice/paymentservice/payment/infrastructure/toss/MockTossPaymentAdapter.java
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/payment/infrastructure/toss/MockTossPaymentAdapter.java
@@ -4,40 +4,58 @@ import com.trustamarket.paymentservice.paymentservice.payment.application.dto.re
 import com.trustamarket.paymentservice.paymentservice.payment.application.port.TossPaymentPort;
 import com.trustamarket.paymentservice.paymentservice.payment.domain.exception.PaymentErrorCode;
 import com.trustamarket.paymentservice.paymentservice.payment.domain.exception.PaymentException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
 import java.util.UUID;
 
 @Component
 @Profile("mocktest")
 public class MockTossPaymentAdapter implements TossPaymentPort {
 
+    @Value("${toss.mock.slow-rate}")
+    private double slowRate;
+
+    @Value("${toss.mock.failure-rate}")
+    private double failureRate;
+
+    private final Random random = new Random();
+
     @Override
     public TossConfirmResult confirm(String paymentKey, UUID paymentId, long amount) {
 
-        if(amount <= 0 ){
+        if (amount <= 0) {
             throw new PaymentException(PaymentErrorCode.INVALID_PAYMENT_AMOUNT);
         }
 
-        //처리 시간 시뮬레이션 2초
-        //결제 승인보다는 결제창에서 실패 확률이 더 높지만 편의성을 위해 confirm으로 실패 테스트 진행
+        simulateDelay();
+
+        if (random.nextDouble() < failureRate) { //PG사 실패 확률
+            throw new PaymentException(PaymentErrorCode.PAYMENT_CONFIRM_UNKNOWN);
+        }
+
+        return new TossConfirmResult(paymentKey, paymentId.toString(), amount);
+    }
+
+    private void simulateDelay() {
+        int delay;
+
+        //일반적으로 1초 이내 처리, 늦을 경우 ~3초까지 걸림
+        if (random.nextDouble() < slowRate) {
+            // 5% - 느린 케이스: 1,000 ~ 3,000ms
+            delay = 1000 + random.nextInt(2000);
+        } else {
+            // 95% - 정상 케이스: 100 ~ 1,000ms
+            delay = 100 + random.nextInt(900);
+        }
+
         try {
-            Thread.sleep(2000);
+            Thread.sleep(delay);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new PaymentException(PaymentErrorCode.PAYMENT_CONFIRM_UNKNOWN);
         }
-
-        //승인실패 5%
-        if (Math.random() <= 0.05) {
-            throw new PaymentException(PaymentErrorCode.PAYMENT_CONFIRM_UNKNOWN);
-        }
-
-        return new TossConfirmResult(
-                paymentKey,
-                paymentId.toString(),
-                amount
-        );
     }
 }
