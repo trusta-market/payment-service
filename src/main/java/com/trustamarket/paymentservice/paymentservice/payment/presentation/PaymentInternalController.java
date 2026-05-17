f0207688b79e35d8b636bf68bcda1031b9d417cd
zlon <zlon@outlook.kr>
2026-05-18 01:09:49 +0900

    refactor: refactor CreatePaymentResponse

--- src/main/java/com/trustamarket/paymentservice/paymentservice/payment/presentation/PaymentInternalController.java
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/payment/presentation/PaymentInternalController.java
@@ -2,9 +2,11 @@ package com.trustamarket.paymentservice.paymentservice.payment.presentation;
 
 import com.trustamarket.common.response.CommonResponse;
 import com.trustamarket.paymentservice.paymentservice.payment.application.dto.command.CreatePaymentCommand;
+import com.trustamarket.paymentservice.paymentservice.payment.application.dto.result.CreatePaymentResult;
 import com.trustamarket.paymentservice.paymentservice.payment.application.port.PaymentUseCase;
-import com.trustamarket.paymentservice.paymentservice.payment.presentation.dto.request.CreatePaymentRequest;
 import com.trustamarket.paymentservice.paymentservice.payment.domain.vo.Amount;
+import com.trustamarket.paymentservice.paymentservice.payment.presentation.dto.request.CreatePaymentRequest;
+import com.trustamarket.paymentservice.paymentservice.payment.presentation.dto.response.CreatePaymentResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,11 +23,12 @@ public class PaymentInternalController {
     private final PaymentUseCase paymentUseCase;
 
     @PostMapping("/charges")
-    public CommonResponse<Void> createPayment(@Valid @RequestBody CreatePaymentRequest request) {
+    public CommonResponse<CreatePaymentResponse> createPayment(@Valid @RequestBody CreatePaymentRequest request) {
 
         CreatePaymentCommand command = new CreatePaymentCommand(request.userId(), request.pointTxRequestHistoryId(), Amount.of(request.chargeAmount()));
-        paymentUseCase.createPayment(command);
+        CreatePaymentResult result = paymentUseCase.createPayment(command);
+        CreatePaymentResponse response = CreatePaymentResponse.from(result);
 
-        return new CommonResponse<>(HttpStatus.NO_CONTENT.value(), null);
+        return new CommonResponse<>(HttpStatus.NO_CONTENT.value(), response);
     }
 }
