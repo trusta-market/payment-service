2d3227146ab554d1e376e264ffe1b0977f87c8b3
zlon <zlon@outlook.kr>
2026-05-20 02:50:38 +0900

    refactor: asyncConfig 수정

--- src/main/java/com/trustamarket/paymentservice/paymentservice/config/AsyncConfig.java
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/config/AsyncConfig.java
@@ -5,18 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
-    @Bean(name = "payoutExecutor")
-    public Executor payoutExecutor() {
+
+    @Bean(name = "payoutTaskExecutor")
+    public ThreadPoolTaskExecutor payoutTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(10);
-        executor.setQueueCapacity(100);
-        executor.setThreadNamePrefix("payout-");
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(50);
+        executor.setQueueCapacity(500);           // 큐 사이즈 늘리기
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy()); // AbortPolicy 대신
+        executor.setThreadNamePrefix("payout-async-");
         executor.initialize();
         return executor;
     }
