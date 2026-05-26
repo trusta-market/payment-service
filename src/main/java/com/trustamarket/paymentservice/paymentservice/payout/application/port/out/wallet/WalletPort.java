cd753b108906fa24838cfc2d0fc0bef44a1a6f68
zlon <zlon@outlook.kr>
2026-05-26 20:02:45 +0900

    refactor: import 제거

--- src/main/java/com/trustamarket/paymentservice/paymentservice/payout/application/port/out/wallet/WalletPort.java
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/payout/application/port/out/wallet/WalletPort.java
@@ -1,7 +1,5 @@
 package com.trustamarket.paymentservice.paymentservice.payout.application.port.out.wallet;
 
-import com.trustamarket.common.response.CommonResponse;
-
 public interface WalletPort {
     void payoutCompleted(PayoutCompletedResult result);
 }
