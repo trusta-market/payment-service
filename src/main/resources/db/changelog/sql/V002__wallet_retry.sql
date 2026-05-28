dfd3751569ba9579f624120c66d85c87d0b0a45a
zlon <zlon@outlook.kr>
2026-05-28 21:13:01 +0900

    chore: retry sql추가

---
+++ src/main/resources/db/changelog/sql/V002__wallet_retry.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS p_wallet_charge_retry (
+    id                          UUID        NOT NULL,
+    payment_id                  UUID        NOT NULL,
+    user_id                     UUID        NOT NULL,
+    point_tx_request_history_id UUID        NOT NULL,
+    payment_status              VARCHAR(20) NOT NULL,
+    amount                      BIGINT      NOT NULL,
+    status                      VARCHAR(20) NOT NULL DEFAULT 'PENDING',
+    retry_count                 INT         NOT NULL DEFAULT 0,
+    last_attempted_at           TIMESTAMPTZ,
+    created_at                  TIMESTAMPTZ,
+    updated_at                  TIMESTAMPTZ,
+    CONSTRAINT pk_p_wallet_charge_retry PRIMARY KEY (id)
+    );
+
+CREATE INDEX idx_wallet_charge_retry_status ON p_wallet_charge_retry (status);
+
+CREATE TABLE IF NOT EXISTS p_wallet_withdraw_retry (
+    id                          UUID        NOT NULL,
+    payout_id                   UUID        NOT NULL,
+    user_id                     UUID        NOT NULL,
+    point_tx_request_history_id UUID        NOT NULL,
+    payout_status               VARCHAR(20) NOT NULL,
+    amount                      BIGINT      NOT NULL,
+    status                      VARCHAR(20) NOT NULL DEFAULT 'PENDING',
+    retry_count                 INT         NOT NULL DEFAULT 0,
+    last_attempted_at           TIMESTAMPTZ,
+    created_at                  TIMESTAMPTZ,
+    updated_at                  TIMESTAMPTZ,
+    CONSTRAINT pk_p_wallet_payout_retry PRIMARY KEY (id)
+    );
+
+CREATE INDEX idx_wallet_withdraw_retry_status ON p_wallet_withdraw_retry (status);
