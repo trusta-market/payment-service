82c36eefbd5147bc0b663040e7065eb456dd6a89
zlon <zlon@outlook.kr>
2026-06-05 00:20:19 +0900

    refactor: 이벤트 제거

--- src/main/java/com/trustamarket/paymentservice/paymentservice/payout/domain/repository/PayoutRepository.java
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/payout/domain/repository/PayoutRepository.java
@@ -2,11 +2,10 @@ package com.trustamarket.paymentservice.paymentservice.payout.domain.repository;
 
 import com.trustamarket.paymentservice.paymentservice.payout.domain.entity.Payout;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PayoutRepository {
     Payout saveAndFlush(Payout payout);
     Payout findById(UUID payoutId);
-    boolean existsByPointTxRequestHistoryId(UUID pointTxRequestHistoryId);
-    Payout save(Payout payout);
 }
