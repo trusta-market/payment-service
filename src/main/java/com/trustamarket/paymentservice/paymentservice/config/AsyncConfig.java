b3e9fdbe118623f6ea7e9d4a8c3b125fab3e9834
zlon <zlon@outlook.kr>
2026-06-11 21:09:31 +0900

    refactor: 가상 스레드 사용

--- src/main/java/com/trustamarket/paymentservice/paymentservice/config/AsyncConfig.java
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/config/AsyncConfig.java
@@ -3,23 +3,16 @@ package com.trustamarket.paymentservice.paymentservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
 
     @Bean(name = "payoutTaskExecutor")
-    public ThreadPoolTaskExecutor payoutTaskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10);
-        executor.setMaxPoolSize(10);
-        executor.setQueueCapacity(500);
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy()); // AbortPolicy 대신
-        executor.setThreadNamePrefix("payout-async-");
-        executor.initialize();
-        return executor;
+    public Executor payoutTaskExecutor() {
+        return Executors.newVirtualThreadPerTaskExecutor();
     }
 }
