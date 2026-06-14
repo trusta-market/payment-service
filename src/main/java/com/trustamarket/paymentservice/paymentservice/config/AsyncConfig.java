73f163613aa8a15a5ec2d51069ef45317dea8834
지원 <140367464+zlonce@users.noreply.github.com>
2026-06-14 18:31:56 +0900

    [refactor] virtual Thread 사용 (#63)
    * chore: retry sql추가
    
    * refactor: 출금 재시도
    
    * refactor: 충전 재시도
    
    * refactor: add slack webhook
    
    * refactor: 재시도 시간, 횟수 수정
    
    * refactor: slack timeout추가
    
    * refactor: 재시도 트랜잭션 분리
    
    * refactor: 첫 지연 재시도 수정
    
    * refactor: 중복 row 예방
    
    * refactor: 인덱스 제거
    
    * refactor: 인터페이스 추가, 디스코드 변경
    
    * refactor: ymal 파일 수정
    
    * refactor: 오류메세지 수정
    
    * refactor: 이름 수정
    
    * refactor: 가상 스레드 사용
    
    * refactor: 가상 스레드 사용

--- src/main/java/com/trustamarket/paymentservice/paymentservice/config/AsyncConfig.java
+++ src/main/java/com/trustamarket/paymentservice/paymentservice/config/AsyncConfig.java
@@ -1,25 +1,9 @@
 package com.trustamarket.paymentservice.paymentservice.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
-
-    @Bean(name = "payoutTaskExecutor")
-    public ThreadPoolTaskExecutor payoutTaskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10);
-        executor.setMaxPoolSize(10);
-        executor.setQueueCapacity(500);
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy()); // AbortPolicy 대신
-        executor.setThreadNamePrefix("payout-async-");
-        executor.initialize();
-        return executor;
-    }
 }
